36aa608374d4d4812bf70e2c5c1f3a861f5cfe22
IlyaPopovLa <il.popov@lamoda.ru>
2024-12-29 16:07:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 import ptbot
+import os
+from dotenv import load_dotenv
+from pytimeparse import parse
 
 
-TG_TOKEN = '7482916524:AAEyJv3EDGGo2k3liTIGNoCF3r3C_nqrLtI'  # подставьте свой
-TG_CHAT_ID = '650796925'  # подставьте свой ID
+load_dotenv()
+TG_TOKEN = os.getenv("TG_TOKEN")
+TG_CHAT_ID = os.getenv("TG_CHAT_ID")
+
+
+def notify_progress(secs_left, time):
+    bot.create_countdown(time, notify_progress)
+    print("Осталось", secs_left)
+
+
+def wait(chat_id, question):
+    time = parse(question)
+    bot.create_timer(time, choose, author_id=chat_id, message=question)
+
+
+def choose(author_id, message):
+    answers = ("Время вышло")
+    bot.send_message(author_id, answers)
+    print("Он спросил:", message)
+    print("Я ответил:", answers)
+
+
 bot = ptbot.Bot(TG_TOKEN)
-bot.send_message(TG_CHAT_ID, "Бот запущен")
+bot.reply_on_message(notify_progress)
+bot.reply_on_message(wait)
+bot.reply_on_message(choose)
+bot.run_bot()
