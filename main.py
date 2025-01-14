c1f0979bb7974adde4758d30737ec1359d0ec561
IlyaPopovLa <il.popov@lamoda.ru>
2025-01-14 16:28:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def wait(bot, tg_chat_id, question):
     bot.send_message(tg_chat_id, "Бот запущен")
     time = parse(question)
     message_id = bot.send_message(tg_chat_id, "Таймер запущен на {} секунд\n{}".format(time, render_progressbar(time, 0)))
-    bot.create_countdown(time, notify_progress, mid=message_id, timer=time)
-    bot.create_timer(time, message_func)
+    bot.create_countdown(time, notify_progress, mid=message_id, timer=time, tg_chat_id=tg_chat_id)
+    bot.create_timer(time, message_func, tg_chat_id=tg_chat_id)
 
 
 def notify_progress(bot, tg_chat_id, secs_left, mid, timer):
@@ -18,8 +18,7 @@ def notify_progress(bot, tg_chat_id, secs_left, mid, timer):
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
-    percent = "{0:.1f}"
-    percent = percent.format(100 * (iteration / float(total)))
+    percent = "{0:.1f}".format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
     pbar = fill * filled_length + zfill * (length - filled_length)
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
